540e08ca34984aee16fe7a8a676a7607db05a8a6
Nicholas Malcolm <bubylou@pm.me>
2024-12-15 12:18:45 -0500

    Fix incorrect links

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,14 +10,14 @@ fi
 SETTINGS_FILE="$APP_DIR/MoriaServerConfig.ini"
 if [[ ! -f $SETTINGS_FILE ]]; then
     echo "No configuration file found, linking default"
-    ln -s $SETTINGS_FILE $CONFIG_DIR/MoriaServerConfig.ini
+    ln -s $CONFIG_DIR/MoriaServerConfig.ini $SETTINGS_FILE
 fi
 
 SAVE_DIR="$APP_DIR/Moria/Saved"
 if [[ ! -d $SAVE_DIR ]]; then
     echo "No save file found, linking directory"
     mkdir -p "$APP_DIR/Moria"
-    ln -s $SAVE_DIR $DATA_DIR
+    ln -s $DATA_DIR $SAVE_DIR
 fi
 
 echo "Starting Moria"
